30769ddbf4ddf55951397df4f910cf1c8a833b4b
surfbrady <yuejiayue@163.com>
2018-01-17 00:05:26 +0800

    m

--- src/main/java/com/supermq/connection/TransportConnection.java
+++ src/main/java/com/supermq/connection/TransportConnection.java
@@ -1,5 +1,6 @@
 package com.supermq.connection;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
@@ -17,6 +18,52 @@ public class TransportConnection {
 	}
 
 	public void doIO(SelectionKey k) {
+		if (k.isReadable()) {
+			try {
+				if (incomingBuffer==lenBuffer) {
+					int num = socketChannel.read(incomingBuffer);
+					if (num<0) {
+						throw new RuntimeException("读取失败");
+					}
+				}
+				if (incomingBuffer.remaining()==0) {
+					incomingBuffer = ByteBuffer.allocate(incomingBuffer.getInt());
+					readPacket();
+				}
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	private void readPacket() {
+		if (incomingBuffer.remaining()!=0) {
+			int num;
+			try {
+				num = socketChannel.read(incomingBuffer);
+				if (num<0) {
+					throw new RuntimeException("读取失败");
+				}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if (incomingBuffer.remaining()==0) {
+			packetReceived();
+			lenBuffer.clear();
+			incomingBuffer = lenBuffer;
+		}
+	}
+
+	/**
+	 * 处理接收到的消息
+	 */
+	private void packetReceived() {
 		// TODO Auto-generated method stub
 		
 	}
